deaf18e67b0645744d2ef91cc72ab2c7f8d686bf
patricijab <patricija.brecko@gmail.com>
2022-03-02 13:15:04 +0100

    fix images

--- volitvomat/src/App.vue
+++ volitvomat/src/App.vue
@@ -24,13 +24,13 @@ import { RouterLink, RouterView } from "vue-router";
     max-width: 720px;
   }
 
-  @media (min-width: 992px) {
-    max-width: 960px;
-  }
+  // @media (min-width: 992px) {
+  //   max-width: 960px;
+  // }
 
-  @media (min-width: 1200px) {
-    max-width: 1140px;
-  }
+  // @media (min-width: 1200px) {
+  //   max-width: 1140px;
+  // }
 }
 
 </style>
